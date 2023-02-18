964bd2d377951e3cba019e3aa15fa2cab23c32ce
Krishnan S <skrishnan2001@gmail.com>
2023-02-18 18:36:45 +0530

    feat: Basic socketIO integration with flask, printing the hashed values of the merkle tree leaves

--- Image_Authentication/Construct_Merkle_Tree.py
+++ Image_Authentication/Construct_Merkle_Tree.py
@@ -6,7 +6,7 @@ def hash_data(data):
     hash_object = hashlib.sha256(str(data).encode('utf-8'))
     return hash_object.hexdigest()
 
-def get_image_chunks(image, chunk_size=1024):
+def get_image_chunks(image, chunk_size=128):
     img = Image.open(io.BytesIO(image))
     width, height = img.size
     chunks = []
@@ -17,6 +17,13 @@ def get_image_chunks(image, chunk_size=1024):
             block.save(chunk, format='JPEG')
             chunks.append(chunk.getvalue())
     return chunks
+
+def get_leaves(chunks):
+    leaves = []
+    for chunk in chunks:
+        leaves.append(hash_data(chunk))
+    return leaves
+
 
 def construct_merkle_tree(chunks): #Recursive Function
     if len(chunks) == 1:
@@ -31,12 +38,14 @@ def construct_merkle_tree(chunks): #Recursive Function
 
 if __name__ == '__main__':
     image_directory_path = 'Images/'
-    image_file_name = 'ISTDept_EncryptedImage'
+    image_file_name = 'Au_ani_00001_tampered'
     ext = '.jpg'
     complete_file_path = image_directory_path + image_file_name + ext
     print('Image File path: ', complete_file_path)
     with open(complete_file_path, 'rb') as f:
         image = f.read()
         chunks = get_image_chunks(image)
+        print("Number of chunks: ", len(chunks))
+        print("Hash of leaf nodes: ", get_leaves(chunks), "\n")
         root = construct_merkle_tree(chunks)
         print("Root Hash:", root)
