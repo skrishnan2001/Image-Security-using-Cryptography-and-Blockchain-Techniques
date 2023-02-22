d9574099b3a2d698833ef038f37278c5696157f1
aravindusj <67594047+aravindusj@users.noreply.github.com>
2023-02-22 15:56:15 +0530

    feat: Integrated smart contracts and merkle tree construction code

--- Blockchain_Integration/Python_Web3_Integration/Merkle_Root_Getter_Setter.py
+++ Blockchain_Integration/Python_Web3_Integration/Merkle_Root_Getter_Setter.py
@@ -1,40 +1,41 @@
 from web3 import Web3, HTTPProvider
 
-def web3_init(infura_endpoint_base, infura_project_id, abi,contract_address):
-  
-  w3 = Web3(HTTPProvider(infura_endpoint_base + infura_project_id))
-  contract = w3.eth.contract(address=contract_address, abi=abi)
-  return w3, contract
+
+def web3_init(infura_endpoint_base, infura_project_id, abi, contract_address):
+    w3 = Web3(HTTPProvider(infura_endpoint_base + infura_project_id))
+    contract = w3.eth.contract(address=contract_address, abi=abi)
+    return w3, contract
 
 
+def getMerkleRootHash(contract, user_id, image_id):
+    return contract.functions.get(user_id, image_id).call()
 
-def getMerkleRootHash(contract,user_id,image_id):
-  return contract.functions.get(user_id,image_id).call()
+
+def getLatestMerkleRootHash(contract, user_id):
+    return contract.functions.get_latest_hash(user_id).call()
 
 
-  
-def setMerkleRootHash(web3, contract, acct_private_key, user_id,hash_value):
+def setMerkleRootHash(web3, contract, acct_private_key, user_id, hash_value):
+    account = web3.eth.account.privateKeyToAccount(acct_private_key)
 
-  account = web3.eth.account.privateKeyToAccount(acct_private_key)
+    # Create a transaction object
+    nonce = web3.eth.getTransactionCount(account.address)
+    gas_price = web3.eth.gasPrice
+    gas_limit = 500000
+    tx = contract.functions.set(user_id, hash_value).buildTransaction(
+        {
+            "from": account.address,
+            "nonce": nonce,
+            "gasPrice": gas_price,
+            "gas": gas_limit,
+        }
+    )
 
-  # Create a transaction object
-  nonce = web3.eth.getTransactionCount(account.address)
-  gas_price = web3.eth.gasPrice
-  gas_limit = 500000
-  tx = contract.functions.set(user_id,hash_value).buildTransaction({
-      "from": account.address,
-      "nonce": nonce,
-      "gasPrice": gas_price,
-      "gas": gas_limit
-  })
+    # Sign the transaction with the account's private key
+    signed_tx = account.signTransaction(tx)
 
-  # Sign the transaction with the account's private key
-  signed_tx = account.signTransaction(tx)
+    # Send the signed transaction to the network
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
-  # Send the signed transaction to the network
-  tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
-  receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-  print(receipt)
-    
-
+    receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+    print(receipt)
--- Blockchain_Integration/Python_Web3_Integration/main.py
+++ Blockchain_Integration/Python_Web3_Integration/main.py
@@ -1,34 +1,58 @@
-from Merkle_Root_Getter_Setter import web3_init, getMerkleRootHash, setMerkleRootHash
+from Merkle_Root_Getter_Setter import (
+    web3_init,
+    setMerkleRootHash,
+    getLatestMerkleRootHash,
+)
 import json
 import os
 from dotenv import load_dotenv
+import sys
+
+sys.path.append("../../")
+from Image_Authentication.Construct_Merkle_Tree import (
+    construct_merkle_tree,
+    get_image_chunks,
+    get_leaves,
+)
+
 
 if __name__ == "__main__":
-  
-  with open("HashStorage.json") as f:
-    info_json = json.load(f)
-  abi = info_json["abi"]
+    with open("HashStorage.json") as f:
+        info_json = json.load(f)
+    abi = info_json["abi"]
 
-  load_dotenv()
+    load_dotenv()
 
-  contract_address = os.getenv('CONTRACT_ADDRESS')
-  infura_project_id = os.getenv('INFURA_PROJECT_ID')
-  infura_endpoint = os.getenv('INFURA_ENDPOINT')
-  acct_private_key = os.getenv('ACCT_PRIVATE_KEY')
+    contract_address = os.getenv("CONTRACT_ADDRESS")
+    infura_project_id = os.getenv("INFURA_PROJECT_ID")
+    infura_endpoint = os.getenv("INFURA_ENDPOINT")
+    acct_private_key = os.getenv("ACCT_PRIVATE_KEY")
 
+    web3, contract = web3_init(
+        infura_endpoint, infura_project_id, abi, contract_address
+    )
 
-  web3, contract = web3_init(infura_endpoint, infura_project_id,abi,contract_address)
+    # Getting value from contract
+    sample_user = "skrishnan2001"
 
-  #Getting value from contract
-  sample_user = "skrishnan2001"
-  sample_hash = "a286855f63aa50765faa5a60951b1229a33188ba40a306f9e52f7fca1b60e74c"
-  
+    # Getting the hash of encrypted image
+    image_directory_path = "Images/"
+    image_file_name = "Au_ani_00001"
+    ext = ".jpg"
+    complete_file_path = image_directory_path + image_file_name + ext
+    print("Image File path: ", complete_file_path)
+    with open(complete_file_path, "rb") as f:
+        image = f.read()
+        chunks = get_image_chunks(image)
+        print("Number of chunks: ", len(chunks))
+        print("Hash of leaf nodes: ", get_leaves(chunks), "\n")
+        image_hash = construct_merkle_tree(chunks)
+        print("Root Hash:", image_hash)
 
-  #Updating the value 
-  #setMerkleRootHash(web3, contract, acct_private_key, sample_user,sample_hash)
+    # Updating the value
+    setMerkleRootHash(web3, contract, acct_private_key, sample_user, image_hash)
 
-  #Retrieving the updated value
-  idx = 1
-  hash_value = getMerkleRootHash(contract,sample_user,idx)
+    # Retrieving the updated value
+    hash_value = getLatestMerkleRootHash(contract, sample_user)
 
-  print("Hash value for image ID  ", idx, " belonging to user ID ", sample_user, " is ", hash_value) 
+    print("Hash value for the latest image belonging to user ID ", sample_user, " is ",hash_value)
